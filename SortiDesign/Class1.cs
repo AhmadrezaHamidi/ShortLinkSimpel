07c4eb45c08b9b569ddd2bdc32e35239e34b8c85
ahmadhfr <vestaabner@gmail.com>
2024-01-02 00:04:45 +0330

    Strategy

---
+++ SortiDesign/Class1.cs
@@ -0,0 +1,96 @@
+﻿namespace SortiDesign;
+
+
+public class Prodeuct
+{
+    public Prodeuct()
+    {
+            
+    }
+
+    private Prodeuct(string name, long prise)
+    {
+        Name = name;
+        Prise = prise;
+    }
+
+    public string Name { get; set; }
+    public long Prise { get; set; }
+    
+    public List<Prodeuct> GetProductes()
+    {
+        var res = new List<Prodeuct>();
+        for (int i = 0; i < 100; i++)
+        {
+            res.Add(new Prodeuct($"name{i}" ,i+956 ));
+        }
+
+        return res;
+    }   
+
+}
+
+public interface ISortEstertegi
+{
+    public List<Prodeuct> Sort(List<Prodeuct> products );
+}
+
+public class SortByName : ISortEstertegi
+{
+    public List<Prodeuct> Sort(List<Prodeuct> products )
+    {
+         products.Sort((product1, product2) => product1.Name.CompareTo(product2.Name));
+         return products;      
+    }
+}
+
+
+
+public class SortById : ISortEstertegi
+{
+    public List<Prodeuct> Sort(List<Prodeuct> products )
+    {
+         products.Sort((product1, 
+             product2) => product1.Prise.CompareTo(product2.Prise));
+         return products;      
+    }
+}
+
+
+public class SortPeodustes
+{
+    private ISortEstertegi _sortEstertegi;
+
+    public SortPeodustes(ISortEstertegi sortEstertegi)
+    {
+        this._sortEstertegi = sortEstertegi;
+    }
+    public List<Prodeuct> Sort(List<Prodeuct> products )
+    {
+        return _sortEstertegi.Sort(products);
+    }
+}
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var pro = new Prodeuct();
+
+        var pros = pro.GetProductes();
+
+        var sort = new SortById();
+        
+        var navigator = new SortPeodustes(sort);
+
+       var ttt =  navigator.Sort(pros);
+
+        foreach (var VARIABLE in ttt)
+        {
+            Console.WriteLine(VARIABLE.Prise);
+        }
+    }
+}
+
+
---
+++ StrategyiDesign/Class1.cs
@@ -0,0 +1,70 @@
+﻿namespace StrategyiDesign;
+
+
+
+
+
+public interface IRouteStrategy
+{
+    public string CalculateRoute(string start, string End);
+}
+
+
+public class CarStrategy : IRouteStrategy
+{
+    public string CalculateRoute(string start, string end)
+    {
+        return $"Route calculated for car from {start} to {end}";
+    }
+}
+
+public class TrainStrategy : IRouteStrategy
+{
+    public string CalculateRoute(string start, string end)
+    {
+        return $"Route calculated for train from {start} to {end}";
+    }
+}
+
+public class WalkingStrategy : IRouteStrategy
+{
+    public string CalculateRoute(string start, string end)
+    {
+        return $"Route calculated for walking from {start} to {end}";
+    }
+}
+
+
+public class Navigator
+{
+    private IRouteStrategy _strategy;
+
+    public void SetStrategy(IRouteStrategy strategy)
+    {
+        _strategy = strategy;
+    }
+
+    public string CalculateRoute(string start, string end)
+    {
+        return _strategy.CalculateRoute(start, end);
+    }
+}
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var navigator = new Navigator();
+
+        // انتخاب استراتژی رانندگی با اتومبیل
+        navigator.SetStrategy(new CarStrategy());
+        Console.WriteLine(navigator.CalculateRoute("خانه", "دفتر کار"));
+
+        // تغییر استراتژی به قطار
+        navigator.SetStrategy(new TrainStrategy());
+        Console.WriteLine(navigator.CalculateRoute("خانه", "دانشگاه"));
+    }
+}
+
+
