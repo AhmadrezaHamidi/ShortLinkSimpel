ea34e610f8c0e3e472387351592cf3c268f921cc
ahmadhfr <vestaabner@gmail.com>
2024-01-03 15:00:12 +0330

    adapter

---
+++ AdapterSample/Class1.cs
@@ -0,0 +1,65 @@
+﻿namespace AdapterSample;
+
+/// <summary>
+/// Adapter is a Structural Design Pattern that allows incompatible interfaces between classes to work together without modifying
+/// their source code.
+/// It acts as a bridge between two interfaces, making them compatible so
+/// that they can collaborate and interact seamlessly
+/// </summary>
+  
+//Change voltaj  
+public interface ILogger
+{
+    void WrtiteDebug(string message);
+    void WrtiteInfo(string message);
+    void WrtiteWarning(string message);
+}
+
+
+public class SeriLogAdapter : ILogger 
+{
+    public void WrtiteDebug(string message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void WrtiteInfo(string message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void WrtiteWarning(string message)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
+
+public class Node4nNetAdapter : ILogger
+{
+    
+    /// <summary>
+    ///  ..Log4net
+    /// </summary>
+    /// <param name="message"></param>
+    /// <exception cref="NotImplementedException"></exception>
+    
+    public void WrtiteDebug(string message)
+    {
+    //Log$NetOgger.
+    }
+
+    public void WrtiteInfo(string message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void WrtiteWarning(string message)
+    {
+        throw new NotImplementedException();
+    }
+} 
+
+
+ 
