f697a7be6e9263b9ab33e87790cb0e66a5517ef6
ahmadhfr <vestaabner@gmail.com>
2024-01-03 12:53:35 +0330

    sampleSteratego

---
+++ strategyDesingPatterns/Class1.cs
@@ -0,0 +1,24 @@
+﻿ namespace strategyDesingPatterns;
+
+ public class Basket
+ {
+     public long  TotalPrice { get; set; }
+ 
+     public void ApplayeDisCount(IDisCountStrategi disCount)
+     {
+         //...
+         ///...TotalPrice
+         this.TotalPrice -= disCount.Calculate(200);
+     }
+ }
+ public class VipCustomerOff : IDisCountStrategi
+  {
+      public long Calculate(long totalPrice)
+      {
+          return totalPrice % 2;
+      }
+  }
+ public interface IDisCountStrategi
+ {
+     long Calculate(long totalPrice);
+ }
