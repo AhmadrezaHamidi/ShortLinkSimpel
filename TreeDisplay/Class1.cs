9c320e9def9d5b46594f476e25215c6b478153de
ahmadhfr <vestaabner@gmail.com>
2024-01-05 17:57:10 +0330

    dd

---
+++ TreeDisplay/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace TreeDisplay;
+
+public class Tree
+{
+}
+
+// public class TreeDisPlay
+// {
+//   public void DisPlay(Tree tree);
+// }
+//
+
+
+public class TrrDisPlay
+{
+    //For Insert file on this 
+    //We must write adapter 
+    public void DISPLAY(Tree tree)
+    {
+        
+    }
+}
